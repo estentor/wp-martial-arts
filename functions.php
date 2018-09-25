1f678da1158ccf6cd989368044233438c2491b55
Flia. Soto Gonzalez <luissoto1984@gmail.com>
2018-09-24 21:20:08 -0300

    cambios functions thumbs

--- functions.php
+++ functions.php
@@ -3,4 +3,10 @@ register_nav_menus(
     array(
         'menu' => 'Main menu',
     ));
+
+
+// Post Thumbnails
+add_theme_support('post-thumbnails');
+add_image_size('team_thumbs', 370,230, true);
+add_image_size();
 ?>
